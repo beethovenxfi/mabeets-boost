742dcdcc4dd165666ff5647715dc7c215786069a
Daniel <danielBeets@protonmail.com>
2025-03-21 16:19:31 +0100

    rename tests, remove console

--- src/MaBeetsBoost.sol
+++ src/MaBeetsBoost.sol
@@ -6,7 +6,6 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {console} from "forge-std/console.sol";
 
 /**
  * @title MaBeetsBoost
